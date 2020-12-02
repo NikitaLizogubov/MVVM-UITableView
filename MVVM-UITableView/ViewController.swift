ee6274507eb5ca083a13c499fe579e5bee8121ad
Nikita Lizogubov <nikitalizogubov2000@gmail.com>
2020-12-02 17:41:20 +0200

    Init project

---
+++ MVVM-UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-UITableView
+//
+//  Created by Nikita Lizogubov on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
